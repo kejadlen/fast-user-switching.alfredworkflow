1ab160325dbde00cd1c1b58d33fa9e382b9af9bb
Alpha Chen <alpha@kejadlen.dev>
2020-03-03 08:20:30 -0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,72 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::PathBuf;
+use std::process::Command;
+use std::result;
+
+use alphred::{workflow_cache, Item};
+use anyhow::{anyhow, Result};
+use serde_json::json;
+
+// Ported from https://github.com/aiyodk/Alfred-Extensions
+
+fn main() -> Result<()> {
+    // let guest_enabled = run("defaults", &["read", "/Library/Preferences/com.apple.loginwindow", "GuestEnabled"])?.parse::<usize>()? == 1;
+    // let me = run("whoami", &[])?;
+    let output = run(
+        "dscl",
+        &[".", "-list", "/Users", "_writers_UserCertificate"],
+    )?;
+    let users: Vec<_> = output
+        .lines()
+        .map(|l| l.split_whitespace().next())
+        .collect::<Option<_>>()
+        .ok_or_else(|| anyhow!("user"))?;
+
+    let mut items = vec![];
+    for user in users {
+        let icon_path = icon(user).unwrap_or_else(|_| PathBuf::from("icon.png"));
+        let item = Item::new(user).arg(user).icon(icon_path.as_path());
+        items.push(item);
+    }
+    println!("{}", json!({ "items": items }));
+
+    Ok(())
+}
+
+fn run(program: &str, args: &[&str]) -> Result<String> {
+    let stdout = Command::new(program).args(args).output()?.stdout;
+    let output = String::from_utf8(stdout)?.trim().to_string();
+    Ok(output)
+}
+
+fn icon(username: &str) -> Result<PathBuf> {
+    let photo = run(
+        "dscl",
+        &[".", "-read", &format!("/Users/{}/", username), "JPEGPhoto"],
+    )?;
+    // fallback: dscl . -read "/Users/$USERNAME/" Picture
+
+    let hex: Vec<_> = photo
+        .lines()
+        .last()
+        .ok_or_else(|| anyhow!(""))?
+        .replace(" ", "")
+        .chars()
+        .collect();
+
+    let binary: Vec<_> = hex
+        .chunks(2)
+        .map(|w| u8::from_str_radix(&format!("{}{}", w[0], w[1]), 16))
+        .collect::<result::Result<_, _>>()?;
+
+    // create dir if it doesn't exist
+    let mut path = workflow_cache()?;
+    path.push(username);
+    path.set_extension("jpg");
+
+    let mut buffer = File::create(&path)?;
+    buffer.write_all(&binary)?;
+
+    Ok(path)
 }
